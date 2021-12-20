7426930aab4427919fe40acf454701ac712456f2
Pär Törnell <paer@ijob.se>
2021-12-20 19:48:51 +0100

    Initial commit

---
+++ src/testing/lexicon/Main.java
@@ -0,0 +1,10 @@
+package testing.lexicon;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int myResult = (23+11+77)/3;
+    System.out.println("Average "+myResult);
+
+    }
+}
